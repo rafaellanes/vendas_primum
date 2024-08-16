b345c9694c1bec413ffd3026c1007cfb4f1ec3b1
rafaellanes <rafael.lanes@primum.edu.br>
2024-08-16 01:15:00 -0300

    u: trazendo as variaveis de ambiente para acessar o bd no postgresql

--- guru_transaction.py
+++ guru_transaction.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DB_USER = os.getenv('DB_USER')
-
-print(DB_USER)
+DB_USER     = os.getenv('DB_USER')
+DB_HOST     = os.getenv('DB_HOST')
+DB_PASSWORD = os.getenv('DB_PASSWORD')
+DB_TABLE    = os.getenv('DB_TABLE')
